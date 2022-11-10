44ccbfcd6c02869b1f003f20e35d7a96415a1165
Eugene <77lm@mail.ru>
2022-11-11 02:58:40 +0600

    Commit Message

--- main.go
+++ main.go
@@ -2,18 +2,19 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	http.HandleFunc("/", hello)
-	err := http.ListenAndServe(":443", nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
-}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to the Internet! Please follow me.\n")
+	})
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Привет!")
+	// создадим переменные APP_IP и APP_PORT, а их значения возьмем из одноименных переменных окружения
+	APP_IP := os.Getenv("APP_IP")
+	APP_PORT := os.Getenv("APP_PORT")
+
+	fmt.Println(APP_IP + ":" + APP_PORT)
+	http.ListenAndServe(APP_IP+":"+APP_PORT, nil)
 }
