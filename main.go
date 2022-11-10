12e1d94e3b90da914faf11379cfa4bd295638934
Eugene <77lm@mail.ru>
2022-11-11 03:37:11 +0600

    Commit Message

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
 )
 
 func main() {
@@ -11,10 +10,5 @@ func main() {
 		fmt.Fprintf(w, "Welcome to the Internet! Please follow me.\n")
 	})
 
-	// создадим переменные APP_IP и APP_PORT, а их значения возьмем из одноименных переменных окружения
-	APP_IP := os.Getenv("APP_IP")
-	APP_PORT := os.Getenv("APP_PORT")
-
-	fmt.Println(APP_IP + ":" + APP_PORT)
-	http.ListenAndServe(APP_IP+":"+APP_PORT, nil)
+	http.ListenAndServe(":8000", nil)
 }
