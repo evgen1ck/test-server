efea5e7b1bb98fa685a67651031f8bc30f028a7a
Eugene <77lm@mail.ru>
2022-11-11 01:29:10 +0600

    Commit Message

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", hello)
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":80", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
