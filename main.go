a22e3deda87a833b6442854cd6c24cbf4f594b5d
Eugene <77lm@mail.ru>
2022-11-13 03:56:32 +0600

    Commit Message

--- main.go
+++ main.go
@@ -17,21 +17,3 @@ func main() {
 
 	http.ListenAndServeTLS(":9990", "/etc/ssl/certs/digitalshop-evgenick-com.pem", "/etc/ssl/private/digitalshop-evgenick-com.key", nil)
 }
-
-//package main
-//
-//import (
-//	"net/http"
-//)
-//
-//func HelloSSLServer(w http.ResponseWriter, r *http.Request) {
-//	w.Header().Set("Content-Type", "text/plain")
-//	w.Write([]byte("This is an example server.\n"))
-//	// fmt.Fprintf(w, "This is an example server.\n")
-//	// io.WriteString(w, "This is an example server.\n")
-//}
-//
-//func main() {
-//	http.HandleFunc("/", HelloSSLServer)
-//	http.ListenAndServeTLS("localhost:9990", "/etc/ssl/digitalshop-evgenick-com.crt", "/etc/ssl/digitalshop-evgenick-com.key", nil)
-//}
